0fe4431c0d631d87b04af570f09d4019d798f68b
LBCHTS <47186072+LBCHTS@users.noreply.github.com>
2019-05-22 11:32:40 +0200

    Fix 7.3 activity management

--- src/main/resources/db/migration/R__activity_management.sql
+++ src/main/resources/db/migration/R__activity_management.sql
@@ -1,21 +1,35 @@
-CREATE OR REPLACE FUNCTION add_activity(in_title character varying(200),in_description text,in_owner_id bigint) RETURNS activity AS $$
+CREATE OR REPLACE FUNCTION add_activity(in_title character varying(200),in_description text,in_owner_id bigint default null) RETURNS activity AS $$
 	DECLARE
 		defaultOwner "user"%rowtype;
-		id_owner bigint;
+		id_new_activity bigint;
 		new_activity activity%rowtype;
 	BEGIN
-		defaultOwner = getDefaultOwner();
+		defaultOwner := get_default_owner();
+		id_new_activity := nextval('id_generator');
 		
 		IF in_owner_id IS NULL THEN
-			id_owner = defaultOwner.id;
-		ELSE
-			id_owner = in_owner_id;
+			in_owner_id := defaultOwner.id;
 		END IF; 
 		
 		INSERT INTO activity (id, title, description, creation_date, modification_date, owner_id)
-		VALUES (nextval('id_generator'), in_title, in_description, now(), now(), id_owner)
-		RETURNING * INTO new_activity;
+		VALUES (id_new_activity, in_title, in_description, now(), now(), in_owner_id);
+		
+		SELECT * INTO new_activity
+		FROM activity
+		WHERE id = id_new_activity;
 		
 		RETURN new_activity;	
 	END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION find_all_activities(activities_cursor refcursor) RETURNS refcursor AS $$
+BEGIN
+	OPEN activities_cursor FOR
+        SELECT act.id AS id, title, description, creation_date, modification_date, owner_id, username
+        FROM activity act 
+        LEFT JOIN "user" owner
+        ON act.owner_id = owner.id
+        ORDER BY title, username;
+    RETURN activities_cursor;
+END
 $$ LANGUAGE plpgsql;
